eb9844068634c2c467f8569f18ddadbc9a795197
BeyondOrigins <dima.a.ivlev@gmail.com>
2024-01-09 22:43:41 +0300

    finished ship class

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from db import db
 from flask_sqlalchemy import *
 
+
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
@@ -20,19 +21,34 @@ class Admin(db.Model):
         return f"<Admin {self.id}>"
 
 
-class Fields(db.Model):
+class Field(db.Model):
     __tablename__ = "field"
+    id = db.Column(db.Integer, primary_key=True)
     size = db.Column(db.Integer, nullable=False)
+    ships = db.relationship("Ship", backref="filed", lazy=True)
 
     def __repr__(self):
         return f"<Field {self.id}>"
 
 
+class Ship(db.Model):
+    __tablename__ = "ship"
+    id = db.Column(db.Integer, primary_key=True)
+    pos_x = db.Column(db.Integer, nullable=False)
+    pos_y = db.Column(db.Integer, nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+    orientation = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return f"<Ship {self.id}>"
+
+
 class Prize(db.Model):
     __tablename__ = "prize"
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     desc = db.Column(db.String, nullable=False)
-    image = db.relationship("Image", backref="prize", lazy=True)
+    image = db.relationship("Image", backref="prize", uselist=False)
 
     def __repr__(self):
         return f"<Prize {self.id}>"
@@ -40,10 +56,11 @@ class Prize(db.Model):
 
 class Image(db.Model):
     __tablename__ = "image"
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     mimetype = db.Column(db.String, nullable=False)
     image = db.Column(db.String, nullable=False)
-    prize_id = db.Column(db.Integer, db.Foreignkey("prize.id"), nullable=False)
+    prize_id = db.Column(db.Integer, db.Foreignkey("prize.id"), nullable=False, unique=True)
 
     def __repr__(self):
         return f"<Image {self.id}>"
